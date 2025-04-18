c26a2d36655bbbc8cb4d439b9c3f8b750fe33a87
Pawel Kowalik <pawelkowalik2@gmail.com>
2025-04-18 15:19:14 +0200

    bare looking app

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-## TEST
+import streamlit as st
+import pandas as pd
+from dotenv import dotenv_values
+from openai import OpenAI
+from hashlib import md5
+# from pycaret.clustering import load_model
+from audiorecorder import audiorecorder
+# from ydata_profiling import ProfileReport
+from pydub import AudioSegment
+from io import BytesIO
+import json
+import plotly.express as px
+import dtale
+# from qdrant_client import QdrantClient
+# from qdrant_client.models import PointStruct, Distance, VectorParams
+
+env = dotenv_values('.env')
+
+df = pd.read_csv('welcome_survey_simple_v2.csv', sep=';')
+
+l, c, r = st.columns([2,6,2])
+with c:
+    st.title('Alike')
+    st.markdown('### Social matching, but smarter.')
+
+tab1, tab2, tab3 = st.tabs(['tab1', 'Ydata raport', 'D-Tale'])
+
+with tab3:
+    d = dtale.show(df)
+    st.write("Open D-Tale in a new tab:")
+    st.markdown(f"[Click here to explore your data]({d._main_url})")
